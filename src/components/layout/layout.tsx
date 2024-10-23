5d382e93c388b43b8d5e7cfa253e654263e55971
AmirxonEngineer <amirshohxaitov98@gmail.com>
2024-10-23 12:22:19 +0500

    all

---
+++ src/components/layout/layout.tsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+<div className="border-blue-500 box-border ">
+    
+<div className="navbar  justify-center mt-10 mb-10 ">
+  <a className="btn bg-blue-200 text-4xl text-indigo-400">daisyUI</a>
+</div>
+</div>
+
+  )
+}
+export default Navbar;
